f3002332ac973f535e30d579c51dc6488e1e767e
Dudlileif <dudlileif@gmail.com>
2023-04-10 12:28:48 +0200

    Tweens now behave as actual tweens.

--- lib/utilities/lat_lng_bounds_tween.dart
+++ lib/utilities/lat_lng_bounds_tween.dart
@@ -1,33 +1,32 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:satreelight/utilities/lat_lng_tween.dart';
 
 /// A class for tweening two [LatLngBounds].
 ///
 /// Get the [LatLngBounds] at any given fraction value with [evaluate].
-class LatLngBoundsTween {
-  const LatLngBoundsTween({required this.begin, required this.end});
+class LatLngBoundsTween extends Tween<LatLngBounds> {
+  /// Creates a [LatLngBounds] tween.
+  ///
+  /// The [begin] and [end] properties must be non-null before the tween is
+  /// first used, but the arguments can be null if the values are going to be
+  /// filled in later.
+  LatLngBoundsTween({super.begin, super.end});
 
-  /// The starting bounds/position.
-  final LatLngBounds begin;
-
-  /// The ending bounds/position.
-  final LatLngBounds end;
-
-  /// Returns the [LatLngBounds] in-between bounds/position for [value].
-  /// [value] is divided by [maxValue] and then clamped between 0 and 1
-  /// in case maxValue isn't set.
-  LatLngBounds evaluate(double value, {double maxValue = 1}) {
-    final clampedValue = (value / maxValue).clamp(0.0, 1.0);
-
+  // The inherited lerp() function doesn't work with LatLngBounds because it is
+  // a complex object with two LatLng objects that individually needs to be
+  // interpolated.
+  @override
+  LatLngBounds lerp(double t) {
     final northWest = LatLngTween(
-      begin: begin.northWest,
-      end: end.northWest,
-    ).evaluate(clampedValue);
+      begin: begin!.northWest,
+      end: end!.northWest,
+    ).transform(t);
 
     final southEast = LatLngTween(
-      begin: begin.southEast,
-      end: end.southEast,
-    ).evaluate(clampedValue);
+      begin: begin!.southEast,
+      end: end!.southEast,
+    ).transform(t);
 
     return LatLngBounds(
       northWest,
--- lib/utilities/lat_lng_tween.dart
+++ lib/utilities/lat_lng_tween.dart
@@ -1,28 +1,24 @@
+import 'package:flutter/animation.dart';
 import 'package:latlong2/latlong.dart';
 
 /// A class for tweening two [LatLng].
 ///
 /// Get the [LatLng] at any given fraction value with [evaluate].
-class LatLngTween {
-  const LatLngTween({required this.begin, required this.end});
+class LatLngTween extends Tween<LatLng> {
+  /// Creates a [LatLng] tween.
+  ///
+  /// The [begin] and [end] properties must be non-null before the tween is
+  /// first used, but the arguments can be null if the values are going to be
+  /// filled in later.
+  LatLngTween({super.begin, super.end});
 
-  /// The starting position.
-  final LatLng begin;
+  // The inherited lerp() function doesn't work with LatLng because it is a
+  // complex object with two doubles that individually needs to be interpolated.
+  @override
+  LatLng lerp(double t) {
+    final latitude = begin!.latitude * (1 - t) + end!.latitude * t;
 
-  /// The ending position.
-  final LatLng end;
-
-  /// Returns the [LatLng] in-between position for [value].
-  /// [value] is divided by [maxValue] and then clamped between 0 and 1
-  /// in case maxValue isn't set.
-  LatLng evaluate(double value, {double maxValue = 1}) {
-    final clampedValue = (value / maxValue).clamp(0.0, 1.0);
-
-    final latitude =
-        begin.latitude * (1 - clampedValue) + end.latitude * clampedValue;
-
-    final longitude =
-        begin.longitude * (1 - clampedValue) + end.longitude * clampedValue;
+    final longitude = begin!.longitude * (1 - t) + end!.longitude * t;
 
     return LatLng(latitude, longitude);
   }
